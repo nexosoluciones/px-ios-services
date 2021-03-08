dbcd5311ad631976a53e9afd06205c4caa069da0
Nahuel Bea <nahub96@gmail.com>
2021-03-08 12:50:04 -0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -21,6 +21,6 @@ let package = Package(
     // Targets can depend on other targets in this package, and on products in packages which this package depends on.
     .target(
         name: "px-ios-services",
-        dependencies: [])
+        path: "MercadoPagoServices")
   ]
 )
